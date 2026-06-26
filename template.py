07018cc38479475dda0a32496dd9a14aa99bbe80
Mohamed Mahmoud <mohamedmahmoud8801@gmail.com>
2026-06-27 02:20:15 +0300

    add new file to structure file

--- template.py
+++ template.py
@@ -19,6 +19,7 @@ list_of_files = [
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
     f"{project_name}/entity/artifact_entiy.py",
+    f"{project_name}/exception/__init__.py",
     f"{project_name}/logger/__init__.py",
     f"{project_name}/pipline/__init__.py",
     f"{project_name}/pipline/training_pipline.py",
