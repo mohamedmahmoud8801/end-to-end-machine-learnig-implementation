4da71ca879c1bf9d4266676c8071b6eee4a5ec62
Mohamed Mahmoud <mohamedmahmoud8801@gmail.com>
2026-06-27 03:12:53 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="mohamed",
+    author_email="mohamedmahmoud8801@gmail.com",
+    packages=find_packages()
+      )
