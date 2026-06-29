adfd7746e203ba2d2c1d9feff66bad1b2cd6a2ed
Mohamed Mahmoud <mohamedmahmoud8801@gmail.com>
2026-06-29 04:47:17 +0300

    logger exception and utils added

---
+++ demo.py
@@ -0,0 +1,14 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys  
+# logging.info("welcome from mohamed")
+
+
+try:
+    r=3/0
+    print(r)
+
+except Exception as e :
+    logging.info(e)
+    raise USvisaException(e,sys) from e
+r
